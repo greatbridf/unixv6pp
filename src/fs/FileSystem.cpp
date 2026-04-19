ee8ac6ecba0c7dfea0659a01c74e295c008577ed
greatbridf <greatbridf@icloud.com>
2026-04-19 15:06:36 +0800

    fs: remove C++ SuperBlock and BadBlock()
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/fs/FileSystem.cpp
+++ src/fs/FileSystem.cpp
@@ -1,11 +1,5 @@
 #include "FileSystem.h"
 #include "Utility.h"
-#include "New.h"
-#include "Kernel.h"
-#include "OpenFileManager.h"
-#include "TimeInterrupt.h"
-#include "Video.h"
-#include "fs_defines.h"
 
 extern "C" bool FileSystem_load_super_block();
 extern "C" bool FileSystem_is_readonly(short);
@@ -14,20 +8,6 @@ extern "C" Inode* FileSystem_i_alloc(short);
 extern "C" void FileSystem_i_free(short, int);
 extern "C" Buf* FileSystem_alloc(short);
 extern "C" void FileSystem_free(short, int);
-
-/*==============================class SuperBlock===================================*/
-/* 系统全局超级块SuperBlock对象 */
-SuperBlock g_spb;
-
-SuperBlock::SuperBlock()
-{
-	//nothing to do here
-}
-
-SuperBlock::~SuperBlock()
-{
-	//nothing to do here
-}
 
 /*==============================class FileSystem===================================*/
 FileSystem::FileSystem()
@@ -79,9 +59,4 @@ Buf* FileSystem::Alloc(short dev)
 void FileSystem::Free(short dev, int blkno)
 {
 	FileSystem_free(dev, blkno);
-}
-
-bool FileSystem::BadBlock(SuperBlock *spb, short dev, int blkno)
-{
-	return 0;
 }
--- src/include/FileSystem.h
+++ src/include/FileSystem.h
@@ -8,38 +8,6 @@
 #include <fs_defines.h>
 
 /*
- * 文件系统存储资源管理块(Super Block)的定义。
- */
-class SuperBlock
-{
-	/* Functions */
-public:
-	/* Constructors */
-	SuperBlock();
-	/* Destructors */
-	~SuperBlock();
-
-	/* Members */
-public:
-	int		s_isize;		/* 外存Inode区占用的盘块数 */
-	int		s_fsize;		/* 盘块总数 */
-
-	int		s_nfree;		/* 直接管理的空闲盘块数量 */
-	int		s_free[100];	/* 直接管理的空闲盘块索引表 */
-
-	int		s_ninode;		/* 直接管理的空闲外存Inode数量 */
-	int		s_inode[100];	/* 直接管理的空闲外存Inode索引表 */
-
-	int		s_flock;		/* 封锁空闲盘块索引表标志 */
-	int		s_ilock;		/* 封锁空闲Inode表标志 */
-
-	int		s_fmod;			/* 内存中super block副本被修改标志，意味着需要更新外存对应的Super Block */
-	int		s_ronly;		/* 本文件系统只能读出 */
-	int		s_time;			/* 最近一次更新时间 */
-	int		padding[47];	/* 填充使SuperBlock块大小等于1024字节，占据2个扇区 */
-};
-
-/*
  * 文件系统类(FileSystem)管理文件存储设备中
  * 的各类存储资源，磁盘块、外存INode的分配、
  * 释放。
@@ -101,13 +69,6 @@ public:
 	 * @comment 释放存储设备dev上编号为blkno的磁盘块
 	 */
 	void Free(short dev, int blkno);
-
-private:
-	/*
-	 * @comment 检查设备dev上编号blkno的磁盘块是否属于
-	 * 数据盘块区
-	 */
-	bool BadBlock(SuperBlock* spb, short dev, int blkno);
 
 private:
 	// no members
