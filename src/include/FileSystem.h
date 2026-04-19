f42756c320fe173f0dda998348bc9b8c00b2ec59
greatbridf <greatbridf@icloud.com>
2026-04-19 15:13:40 +0800

    fs: inline all filesystem functions
    Signed-off-by: greatbridf <greatbridf@icloud.com>

--- src/include/FileSystem.h
+++ src/include/FileSystem.h
@@ -4,8 +4,17 @@
 #include "INode.h"
 #include "Buf.h"
 #include "BufferManager.h"
+#include "Utility.h"
 
 #include <fs_defines.h>
+
+extern "C" bool FileSystem_load_super_block();
+extern "C" bool FileSystem_is_readonly(short);
+extern "C" void FileSystem_update();
+extern "C" Inode* FileSystem_i_alloc(short);
+extern "C" void FileSystem_i_free(short, int);
+extern "C" Buf* FileSystem_alloc(short);
+extern "C" void FileSystem_free(short, int);
 
 /*
  * 文件系统类(FileSystem)管理文件存储设备中
@@ -25,50 +34,75 @@ public:
 	/* Functions */
 public:
 	/* Constructors */
-	FileSystem();
+	inline FileSystem() {}
 	/* Destructors */
-	~FileSystem();
+	inline ~FileSystem() {}
 
 	/*
 	 * @comment 初始化成员变量
 	 */
-	void Initialize();
+	inline void Initialize()
+	{
+		//nothing to do here
+	}
 
 	/*
 	* @comment 系统初始化时读入SuperBlock
 	*/
-	void LoadSuperBlock();
+	inline void LoadSuperBlock()
+	{
+		if (!FileSystem_load_super_block())
+			Utility::Panic("Load SuperBlock Error....!\n");
+	}
 
 	/*
 	 * @comment 检查文件系统是否只读
 	 */
-	bool IsReadOnly(short dev);
+	inline bool IsReadOnly(short dev)
+	{
+		return FileSystem_is_readonly(dev);
+	}
 
 	/*
 	 * @comment 将SuperBlock对象的内存副本更新到
 	 * 存储设备的SuperBlock中去
 	 */
-	void Update();
+	inline void Update()
+	{
+		FileSystem_update();
+	}
 
 	/*
 	 * @comment  在存储设备dev上分配一个空闲
 	 * 外存INode，一般用于创建新的文件。
 	 */
-	Inode* IAlloc(short dev);
+	inline Inode* IAlloc(short dev)
+	{
+		return FileSystem_i_alloc(dev);
+	}
 	/*
 	 * @comment  释放存储设备dev上编号为number
 	 * 的外存INode，一般用于删除文件。
 	 */
-	void IFree(short dev, int number);
+	inline void IFree(short dev, int number)
+	{
+		FileSystem_i_free(dev, number);
+	}
 
 	/*
 	 * @comment 在存储设备dev上分配空闲磁盘块
 	 */
-	Buf* Alloc(short dev);
+	inline Buf* Alloc(short dev)
+	{
+		return FileSystem_alloc(dev);
+	}
 	/*
 	 * @comment 释放存储设备dev上编号为blkno的磁盘块
 	 */
-	void Free(short dev, int blkno);
+	inline void Free(short dev, int blkno)
+	{
+		FileSystem_free(dev, blkno);
+	}
 
 private:
 	// no members
